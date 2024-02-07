64671a9c7850ba3de826914db9e97abdeb15d12e
Nikos Koikas <n.koikas@kaizengaming.com>
2024-02-08 00:31:33 +0200

    feat(configs): added tailwind & prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+      types: `${path.resolve(__dirname, "./src/types")}`,
+      hooks: `${path.resolve(__dirname, "./src/hooks")}`,
+    }
+  },
+  server: {
+    port: 8080
+  },
   test: {
-    global: true,
+    globals: true,
     environment: 'jsdom',
     setupFiles: './setupTest.ts'
   },
